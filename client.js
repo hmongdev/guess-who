40d3724f3baee0f4d5c26ea0ae637cf190a63cb5
Avery Yang <averyyang2674@gmail.com>
2022-06-20 16:46:39 -0500

    add loop and append

--- client.js
+++ client.js
@@ -2,6 +2,20 @@
 $(ready)
 
 //ready function
-function ready(){
-    console.log(`Hey! It's working!`);
+function ready() {
+    appendDivs();
+}
+
+// On document load, append divs for each person in the people array (it comes from `data.js` file)
+function appendDivs() {
+    //appends the divs for each person
+
+    //for loop
+    for (let i = 0; i < people.length; i++) {
+        console.log(people[i]);
+        //append to DOM
+        $('body').append(`<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris"></img>`);
+        //create divs
+    }
+
 }
